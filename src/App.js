df252b59959cc7360d8727e454ec37084509b1ab
alabi sodiq <adisco4420@gmail.com>
2019-05-20 17:20:58 +0100

    route for setup page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import SignUp from './components/SignUp';
-import SignIn from './components/SignIn';
+import SignUp from './components/auth/sign-up/SignUp';
+import SignIn from './components/auth/sign-in/SignIn';
+import Setup from './components/auth/setup/Setup';
 
 
 
@@ -15,7 +16,8 @@ class App extends Component {
             
             <Route path='/signup' component={SignUp} />
             <Route path='/signin' component={SignIn} />
-          
+            <Route path='/setup' component={Setup} />
+             
             </div>
 
 
