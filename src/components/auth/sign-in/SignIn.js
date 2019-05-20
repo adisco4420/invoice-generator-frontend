aaa87794f711756b7c6e97e4d31313129e68caaf
alabi sodiq <adisco4420@gmail.com>
2019-05-20 17:21:42 +0100

    connect signup page to the api

--- src/components/auth/sign-in/SignIn.js
+++ src/components/auth/sign-in/SignIn.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './SignUp.css';
+import './SignIn.css';
 
 class SignIn extends Component{
     render(){
---
+++ src/components/auth/sign-up/SignUp.js
@@ -0,0 +1,126 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import env from '../../../env'
+import './SignUp.css';
+import {
+    FormBuilder,
+    FieldGroup,
+    FieldControl,
+    Validators
+} from "react-reactive-form";
+const TextInput = ({ handler, touched, hasError, meta }) => (
+    <div className={`${meta.inputClass || "col-md-12"} mb-2`}>
+        <input
+            type={meta.type || 'text'}
+            className="form-control form-control-md"
+            placeholder={`Enter ${meta.label}`}
+            {...handler()}
+        />
+        <span className="text-danger text-monospace" style={{ fontSize: "13px" }}>
+            {touched && hasError("required") && `${meta.label} is required`}
+            {touched && hasError("email") && `${meta.label} is invalid`}
+            {touched && hasError("minLength") && `${meta.label} is invalid`}
+        </span>
+    </div>
+);
+
+class SignUp extends Component {
+    state = {
+        loading: false,
+        errMsg: null
+    }
+    storeToken(token) {
+        localStorage.setItem('Invoice-Reg-Token', token)
+    }
+
+    registerUser = async () => {
+        this.setState({loading: true, errMsg: false})
+        try {
+            const res = await axios.post(`${env.Invoice_API}/user/register`, this.registerForm.value)
+            this.storeToken(res.data.data.token)
+            this.props.history.push('/setup')
+            // console.log(res.data.data);
+        } catch (error) {
+            this.setState({loading: false, errMsg: error.response.data.message})
+            console.log(error.response);
+            
+        }
+    }
+    registerForm = FormBuilder.group({
+        email: ["", [Validators.required, Validators.email]],
+        phoneNumber: ["", [Validators.required, Validators.minLength(11)]],
+        password: ["", Validators.required],
+    });
+    handleSubmit = e => {
+        e.preventDefault()
+        if (this.registerForm.valid) {
+            this.registerUser();
+        }
+    }
+    render() {
+        return (
+            <React.Fragment>
+
+                <div className="container">
+                    <div className="layout">
+                        <div className="formheader">
+                            <h3> Create a new account</h3>
+                            {this.state.loading  ? <i className="fa fa-spinner  fa-spin"></i> : ''}
+                                </div>
+                                {this.state.errMsg && <div className="bg-danger text-center text-light">{this.state.errMsg}</div>}
+                
+                        <FieldGroup
+                            control={this.registerForm}
+                            render={({ get, invalid, submitted, }) => (
+                                <form onSubmit={this.handleSubmit} className="formlayout">
+                                    <div className="row">
+                                        <div className="col-md-12">
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <FieldControl
+                                            name="email"
+                                            render={TextInput}
+                                            meta={{ label: "Email Address" , type: 'email'}}
+                                        />
+
+                                        <FieldControl
+                                            name="phoneNumber"
+                                            render={TextInput}
+                                            meta={{ label: "Phone Number" , type: 'number'}}
+                                        />
+
+                                        <FieldControl
+                                            name="password"
+                                            render={TextInput}
+                                            meta={{ label: "Password", type: 'password' }}
+                                        />
+                                    </div>
+                                    <button disabled={invalid} style={invalid ? { opacity: '0.8' } : { opacity: '1' }} type="submit" className="btn-lg button">
+                                        Register
+                  </button>
+                  <div className="text-center mt-2">Already Registered? <Link to="/signin">Login</Link></div>
+                                </form>
+                            )}
+                        />
+
+                    </div>
+
+
+                </div>
+
+
+
+
+
+
+
+
+
+            </React.Fragment>
+        );
+    }
+}
+
+export default SignUp;
