d9039ececd3bdb5af382d907ae6267e6511b7563
alabi sodiq <adisco4420@gmail.com>
2019-05-22 17:46:33 +0100

    update fix

--- src/components/auth/setup/Setup.js
+++ src/components/auth/setup/Setup.js
@@ -7,6 +7,7 @@ import {
   FieldControl,
   Validators
 } from "react-reactive-form";
+import { brandLogo } from "../../../utils";
 const busCat = ['Individual/Freelancing', 'Accounting & Bookeeping', 'Agriculture', 'Association or Club',
   'Automotive', 'Business Consulting', 'Clinics & Healthcare Services', 'Constructions & Engineering',
   'Education', 'Entertainment and Arts', 'Financial Services', 'Hotels & Hospitality', 'HR & Recruiting',
@@ -174,6 +175,12 @@ class Setup extends React.Component {
        
             </div>
           </div>
+          <div className="row">
+                        <div className="col-md-4 offset-md-4">
+                            <img style={brandLogo.style} alt="icon" src={brandLogo.link}/> 
+                            <h3 className='text-center text-blue'>InvoiceNG</h3>
+                        </div>
+                    </div>
           </div>
       </React.Fragment>
     );
---
+++ src/utils.js
@@ -0,0 +1,7 @@
+export const brandLogo = {
+    link:  'https://cdn0.iconfinder.com/data/icons/e-commerce-and-shopping-2/512/contract_document_agreement_paper_sign_list_signature_treaty_business_securities_certificate_text_page_term_pact_sheet_flat_design_icon-512.png',
+    style: {
+        width: '25%',
+        margin: '10% 40%'
+    }
+}
