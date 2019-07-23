608131fd1bd799d400f9a169b30a9f43b6948ebf
adisco4420 <adisco4420@gmail.com>
2019-07-23 21:18:05 +0100

    protect dashboard route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Switch, Route , Redirect} from 'react-router-dom';
 import './App.css'
 import BasicRoute from './components/core/basic-route';
 import DashRoute from './components/dashboard/dash-routing';
+import ProtectedRoute from './services/protected-route';
 
 
 class App extends Component {
@@ -13,7 +14,8 @@ class App extends Component {
 
           <BrowserRouter>
             <Switch>
-            <Route  path="/dashboard" render={(props) => <DashRoute {...props} />} />
+            <ProtectedRoute path="/dashboard" component={DashRoute} />
+            {/* <Route  path="/dashboard" render={(props) => <DashRoute {...props} />} /> */}
             <Route  path="/" render={(props) => <BasicRoute {...props} />} />
             <Redirect  from="/" exact to="/" />
             <Redirect  to="/not-found" />
