21fcc9c4ee35d3cb2328370f93e37d533b3b5da2
adisco4420 <adisco4420@gmail.com>
2019-07-24 20:04:21 +0100

    update dash home

--- src/components/dashboard/dashHome/DashHome.js
+++ src/components/dashboard/dashHome/DashHome.js
@@ -18,55 +18,67 @@ function bgColor(title) {
 }
 class DashHome extends Component {
     state = {  }
-    fetchActDetail() {
-
-    }
     componentDidMount() {
       
     }
     
     render() { 
-        return ( 
-            <div>
-            <div className="px-4">
-                <div>
-                    <span className="ml-3" style={{ fontSize: '25px' }}>Dashboard</span>
-                    <span className="float-right mr-4">
-                        <button className="btn btn-primary btn-sm mr-2"><i className="fa fa-plus-circle"></i> Add Contact</button>
-                        <button className="btn btn-success btn-sm"><i className="fa fa-plus-circle"></i> Add Invoice</button>
-                    </span>
-                </div>
+        const { accountDetail } = this.props
+        if(!accountDetail) {
+            return (
                 <div className="container">
-                    <div className="row mt-2">
-                        {
-                            accounts.map((item, index) => {
-                                return <div key={index} className="col-md-4">
-                                    <div className="card" style={{ minHeight: '140px' }}>
-                                        <div className="card-body mt-3">
-                                            <h5>{item.balance}</h5>
-                                            <button style={{ padding: '0px 5px' }} className={`btn btn-sm text-light text-lowercase ${bgColor(item.title)}`}>
-                                                {`${item.title} - usd`}
-                                            </button>
-                                            <div className="float-right"><i className={`fa ${item.icon} fa-2x`}></i></div>
+                    <div className="row mt-3">
+                        <div className="col-12 text-center">
+                            <p>no data</p>
+                            {/* <span className="fa fa-spinner fa-spin fa-3x"></span> */}
+                        </div>
+                    </div>
+                </div>
+            )
+        } else {
+            return ( 
+                <div>
+                <div className="px-4">
+                    <div>
+                        <span className="ml-3" style={{ fontSize: '25px' }}>Dashboard</span>
+                        <span className="float-right mr-4">
+                            <button className="btn btn-primary btn-sm mr-2"><i className="fa fa-plus-circle"></i> Add Contact</button>
+                            <button className="btn btn-success btn-sm"><i className="fa fa-plus-circle"></i> Add Invoice</button>
+                        </span>
+                    </div>
+                    <div className="container">
+                        <div className="row mt-2">
+                            {
+                                accounts.map((item, index) => {
+                                    return <div key={index} className="col-md-4">
+                                        <div className="card" style={{ minHeight: '140px' }}>
+                                            <div className="card-body mt-3">
+                                                <h5>{item.balance}</h5>
+                                                <button style={{ padding: '0px 5px' }} className={`btn btn-sm text-light text-lowercase ${bgColor(item.title)}`}>
+                                                    {`${item.title} - usd`}
+                                                </button>
+                                                <div className="float-right"><i className={`fa ${item.icon} fa-2x`}></i></div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            })
-                        }
-                    </div>
-                    <div className="row mt-3">
-                        <div className="col-md-8">
-                            <Graph />>
+                                })
+                            }
                         </div>
-                        <div className="col-md-4">
-                            <Tips />
+                        <div className="row mt-3">
+                            <div className="col-md-8">
+                                <Graph />>
+                            </div>
+                            <div className="col-md-4">
+                                <Tips />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+    
+             );
+        }
 
-         );
     }
 }
  
