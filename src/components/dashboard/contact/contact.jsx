581cb36c9e74bf18992fdd76cf792c12f0b867a1
adisco4420 <adisco4420@gmail.com>
2019-07-27 19:40:07 +0100

    link to contact page

--- src/components/dashboard/contact/contact.jsx
+++ src/components/dashboard/contact/contact.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class Contact extends Component {
-    state = {  }
+
     render() { 
         return ( <p>Conatct Page</p> );
     }
---
+++ src/utilities/navigate.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+
+// const changeLocation = (route) => {
+//     window.location = route
+// }
+
+// const Navigate = ({route, children}) => {
+//     return ( <a href="/dashboard/contact" >{children}</a> );
+// }
+ 
+// export default Navigate; 
