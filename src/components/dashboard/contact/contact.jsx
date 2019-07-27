d4a5da4faea62dda14ad95bb606ac31d452871e2
adisco4420 <adisco4420@gmail.com>
2019-07-27 10:12:32 +0100

    generate contact page

---
+++ src/components/dashboard/contact/contact.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    state = {  }
+    render() { 
+        return ( <p>Conatct Page</p> );
+    }
+}
+ 
+export default Contact;
