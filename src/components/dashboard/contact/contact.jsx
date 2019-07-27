b2db84971c79f876acc723a7a9fcecffabb9482b
adisco4420 <adisco4420@gmail.com>
2019-07-27 20:33:26 +0100

    add image for contact page

--- src/components/dashboard/contact/contact.jsx
+++ src/components/dashboard/contact/contact.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import AddContact from './add-contact';
 
 class Contact extends Component {
 
     render() { 
-        return ( <p>Conatct Page</p> );
+        return ( 
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12  text-center">
+                    <img width="40%" src="https://dashboard.invoice.ng/dboard/img/welcome.png" alt="imag"/>
+                    <h5 className="mt-3">Stay more organised with your business contacts.</h5>
+                    <button data-toggle="modal" data-target="#addContact" className="btn btn-primary"><i className="fa fa-plus-circle"></i> Add Contact</button>
+                </div>
+            </div>
+            <AddContact />            
+        </div> );
     }
 }
  
