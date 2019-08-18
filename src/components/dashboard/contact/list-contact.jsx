949f68c9aba6e67b004a81779f69418fd3ecd214
alabi sodiq <adisco4420@gmail.com>
2019-08-18 13:39:04 +0100

    display date instead of address

--- src/components/dashboard/contact/list-contact.jsx
+++ src/components/dashboard/contact/list-contact.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PageHeader from '../../../common/header';
+import { getDate } from '../../../utilities/basic';
 const ListContact = ({contacts, onDelete}) => {
     return (
+      <React.Fragment>
+      <PageHeader title="Contacts" />
+      {/* <span className="float-right ">
+          <button data-toggle="modal" data-target="#addContact" className="btn btn-primary btn-sm mr-2"><i className="fa fa-plus-circle"></i> Add Contact</button>
+          <button  className="btn btn-success btn-sm"><i className="fa fa-plus-circle"></i> Add Invoice</button>
+      </span> */}
     <table className="table table-hover">
     <thead>
       <tr>
@@ -9,7 +17,7 @@ const ListContact = ({contacts, onDelete}) => {
         <th scope="col">Full Name</th>
         <th scope="col">Email</th>
         <th scope="col">Phone Number</th>
-        <th scope="col">Address</th>
+        <th scope="col">Date</th>
         <th scope="col">Actions</th>        
       </tr>
     </thead>
@@ -20,7 +28,7 @@ const ListContact = ({contacts, onDelete}) => {
                 <td>{contact.fullName}</td>
                 <td>{contact.email || 'null'}</td>
                 <td>{contact.phoneNumber || 'null'}</td>
-                <td>{contact.address || 'null'}</td>
+                <td>{getDate(contact.createdAt) || 'null'}</td>
                 <td>
                     <Link to={`contact/${contact._id}`}><span className="fa fa-eye mr-3" title="open"></span></Link>
                     <button onClick={() => onDelete(contact._id)} className="btn btn-danger btn-sm" title="delete">
@@ -32,7 +40,8 @@ const ListContact = ({contacts, onDelete}) => {
         }) }
 
     </tbody>
-  </table> );
+  </table> 
+  </React.Fragment>);
 }
  
 export default ListContact;
