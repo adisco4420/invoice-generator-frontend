cfc08e3167a633b4f34859f2103d4dd996e6057a
alabi sodiq <adisco4420@gmail.com>
2019-08-18 13:39:23 +0100

    catch error case

--- src/components/dashboard/contact/view-contact.jsx
+++ src/components/dashboard/contact/view-contact.jsx
@@ -1,41 +1,54 @@
-import React, { Component } from 'react';
-import { viewContactSrv } from '../../../services/contact';
-import Loader from '../../../common/loader';
+import React, { Component } from "react";
+import { viewContactSrv } from "../../../services/contact";
+import Loader from "../../../common/loader";
+import PageHeader from "../../../common/header";
+import CardDetail from "../../../common/card-detail";
+import { toast } from 'react-toastify';
 
 class ViewContact extends Component {
-    state = {
-        contact: null
-    }
+  state = {
+    contact: null
+  };
 
-    componentDidMount() {
-       const {id} = this.props.match.params;
-        this.fetchContact(id);
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.fetchContact(id);
+  }
+  async fetchContact(id) {
+    try {
+      const { data } = await viewContactSrv(id);
+      this.setState({ contact: data.data });
+    } catch (error) {
+      toast.error(error.message.message);
+      setTimeout(() => {
+        this.props.history.push('/dashboard/contact')
+      }, 2000);
     }
-   async fetchContact(id) {
-        try {
-            const {data} = await viewContactSrv(id)
-            this.setState({contact: data.data})
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    
-    render() { 
-        const { contact } = this.state;
-        return ( 
-            <div className="container">
-               {!contact && <Loader />}
-                {contact && <div className="row">
-                    <div className="col-md-12">
-                        <header>
-                            <h5 className="d-inline">{contact.fullName}</h5>
-                            <button className="btn btn-info text-right">Edit</button>
-                        </header>
-                    </div>
-                </div>}
+  }
+
+  render() {
+    const { contact } = this.state;
+    return (
+      <div className="container">
+        {!contact && <Loader />}
+        {contact && (
+          <React.Fragment>
+            <header className="mb-1 text-capitalize">
+              <PageHeader title={contact.fullName} />
+              <span className="float-right ">
+                <button className="btn btn-success btn-sm">
+                  <i className="fa fa-edit" /> Edit
+                </button>
+              </span>
+            </header>
+            <div className="row">
+                <CardDetail data={contact.accounts}/>
             </div>
-         );
-    }
+          </React.Fragment>
+        )}
+      </div>
+    );
+  }
 }
- 
+
 export default ViewContact;
