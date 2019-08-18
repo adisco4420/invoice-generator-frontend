ca9e04ebad97673343684664faf36c9f5b39a6a0
alabi sodiq <adisco4420@gmail.com>
2019-08-18 01:05:47 +0100

    view contact componet

--- src/components/dashboard/contact/view-contact.jsx
+++ src/components/dashboard/contact/view-contact.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import { viewContactSrv } from '../../../services/contact';
+import Loader from '../../../common/loader';
 
 class ViewContact extends Component {
+    state = {
+        contact: null
+    }
+
     componentDidMount() {
-        console.log(this.props)
+       const {id} = this.props.match.params;
+        this.fetchContact(id);
+    }
+   async fetchContact(id) {
+        try {
+            const {data} = await viewContactSrv(id)
+            this.setState({contact: data.data})
+        } catch (error) {
+            console.log(error);
+        }
     }
     
-    state = {  }
     render() { 
+        const { contact } = this.state;
         return ( 
             <div className="container">
-                <h5></h5>
+               {!contact && <Loader />}
+                {contact && <div className="row">
+                    <div className="col-md-12">
+                        <header>
+                            <h5 className="d-inline">{contact.fullName}</h5>
+                            <button className="btn btn-info text-right">Edit</button>
+                        </header>
+                    </div>
+                </div>}
             </div>
          );
     }
